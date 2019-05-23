6f1716e4e9e148ac19bccc7310ac455680f6a6e9
Tom Peters <tom.peters@wrnch.ai>
2019-05-23 09:01:41 -0400

    using std::vector for brevity

--- memory-safety/reading-secrets.cc
+++ memory-safety/reading-secrets.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 
+using std::vector;
+
 int main(int const argc, char const **argv) {
   int const index = std::stoi(argv[1]);
 
-  std::vector<char const *> const secret_words1 = {"shh!", "private_key"};
-  std::vector<char const *> const words = {"hello", "world"};
-  std::vector<char const *> const secret_words = {"private",
-                                                  "social insurance no"};
+  vector<char const *> const secret_words1 = {"shh!", "private_key"};
+  vector<char const *> const words = {"hello", "world"};
+  vector<char const *> const secret_words = {"private", "social insurance no"};
 
   std::cout << words[index] << "\n";
 }
